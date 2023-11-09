3fa544f56f4f70612e37fa0694ec240ddef2cfce
Sansriti Sharma <sansriti1999@gmail.com>
2023-11-09 16:17:45 +0530

    image-acrousel

--- image-carousel/src/App.js
+++ image-carousel/src/App.js
@@ -6,9 +6,16 @@ import "./Carousel/carousel.css";
 
 function App() {
 
+  const images = [
+    "https://images.pexels.com/photos/169647/pexels-photo-169647.jpeg?auto=compress&cs=tinysrgb&w=600",
+    "https://images.pexels.com/photos/313782/pexels-photo-313782.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    "https://images.pexels.com/photos/773471/pexels-photo-773471.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    
+  ];
+
   return (
     <div className="App">
-      <Carousel />
+      <Carousel images={images}/>
     </div>
   );
 }
--- image-carousel/src/Carousel/carousel.js
+++ image-carousel/src/Carousel/carousel.js
@@ -1,25 +1,36 @@
-// import { useState } from "react";
+import { useState } from "react";
 // import { motion, AnimatePresence } from "framer-motion";
 
 function Carousel ({images}) {
 
-    // const [currentIndex, setCurrentIndex] = useState(0);
+    const [currentIndex, setCurrentIndex] = useState(0);
 
-    // const handleNext = () => {
-    //     setCurrentIndex(
-    //         (prevIndex) =>
-    //         prevIndex + 1 === images.length ? 0 : prevIndex + 1
-    //     );
+    const handleNext = () => {
+        setCurrentIndex(
+            (prevIndex) =>
+            prevIndex + 1 === images.length ? 0 : prevIndex + 1
+        );
+    }
 
-    //     const handlePrevious = () => {
-    //         setCurrentIndex((prevIndex) =>
-    //           prevIndex - 1 < 0 ? images.length - 1 : prevIndex - 1
-    //         );
-    //       };
-    // }
+    const handlePrevious = () => {
+        setCurrentIndex((prevIndex) =>
+          prevIndex - 1 < 0 ? images.length - 1 : prevIndex - 1
+        );
+    };
 
+    
     return (
-        <p className="red">Works!</p>
+
+        <div className="containerStyles">
+            <p className="title">Image Carousel</p>
+            <div className="imageStyles" style={{backgroundImage: `url(${images[currentIndex]})`}}>
+            </div>
+            <div className="slider-direction">
+                <button className="left arrow" onClick={handlePrevious}><i className="icon"></i></button>
+                <button className="right arrow" onClick={handlePrevious}><i className="icon"></i></button>
+            </div>
+
+        </div>
     );
 }
 
